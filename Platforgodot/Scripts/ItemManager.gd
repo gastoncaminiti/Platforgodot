1a14c9cb1314009101379f4f4b67b8dca2895ffb
gastoncaminiti <gastoncaminiti@outlook.com>
2020-06-19 22:59:29 -0300

    - Platforgodot 0.10 Escena de Inicio, Escena de creditos, Intercambio de Escena. Ultimo item.

--- Platforgodot/Scripts/ItemManager.gd
+++ Platforgodot/Scripts/ItemManager.gd
@@ -22,3 +22,9 @@ func _on_Vida0_body_entered(body):
 			DataManager.set_life_player(1)
 		$AudioStreamPlayer.play()
 		$AnimationPlayer.play("Delete")
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "Delete":
+		if is_in_group("LastItem"):
+			get_tree().change_scene("res://Interfaces/Credit.tscn")
+	queue_free()
